4348ee8502631f8e3b32e0b57a568d215db87455
marie <letenoumariefrance@gmail.com>
2021-12-21 15:47:19 +0100

    modifications_categorie

---
+++ projetGL/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data=Category::latest()->get();
+        return view('admin.categorie.index',compact('data'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.categorie.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data=$request->all();
+        Category::create($data);
+        return redirect('admin/category');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $category= Category::findOrfail($id);
+        return view('admin.categorie.show',compact('category'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $category= Category::findOrfail($id);
+        return view('admin.categorie.create',compact('category'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $category = Category::findOrFail($id);
+        $data=$request->all();
+        $category->update($data);
+        return redirect('admin/category');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $category= Category::findOrfail($id);
+        $category->destroy($id);
+        return redirect('admin/category');
+
+    }
+}
---
+++ projetGL/resources/views/admin/categorie/create.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <form action="{{url(isset($category)?'admin/category/update/'.$category->id:'admin/category/ajout')}}" method="POST">
+            @csrf
+            <div class="form-group col-md-12">
+                <label for="nom" class="control-label">Nom:</label>
+            <input type="text" class="form-control" name="nom_cat" value="{{isset($category)?$category->nom_cat:""}}" required>
+            </div>
+            <div class=" form-group col-md-12">
+                <label for="description" class="control-label">Description:</label>
+                <input type="text" class="form-control" name="description" value="{{isset($category)?$category->description:""}}">
+            </div>
+            <div class="row form-group">
+                <div class="col-md-5"></div>
+                <div class="col-md-4">
+                    <button type="submit" class="btn btn-success">{{isset($category)?'Modifier':'Ajouter'}}</button>
+
+                </div>
+            </div>
+        </form>
+    </div>
+@endsection
